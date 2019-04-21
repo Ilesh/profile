e9a5d2a117251e39b744aa128121e3798982f741
ilesh <ilesh.panchal1991@gmail.com>
2019-04-21 23:55:28 +0530

    => Project section ready.

--- js/modal.js
+++ js/modal.js
@@ -36,10 +36,10 @@ $(document).ready(function(){
         }
     };
 
-    $('#gallery .button').on('click', function(){
+    /*$('#gallery .button').on('click', function(){
         fillModal(this.id);
         $('.modal-wrap').addClass('visible');
-    });
+    });*/
 
     $('.close').on('click', function(){
         $('.modal-wrap, #modal .button').removeClass('visible');
